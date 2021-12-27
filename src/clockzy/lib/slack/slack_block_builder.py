cdbc7e97adf2ff85c6a626a77fe8a437c72de306
jmv74211 <jonathan.martin@wazuh.com>
2021-12-27 21:40:29 +0100

    add: Add new slack message builders

---
+++ src/clockzy/lib/slack/slack_block_builder.py
@@ -0,0 +1,41 @@
+""""Block builder module for slack messages"""
+
+
+def write_slack_divider():
+    """Write a slack divider block.
+
+    Returns:
+        dict: Block message
+    """
+    return {
+        "type": "divider"
+    }
+
+
+def write_slack_markdown(message, image_url=None, alt_text='generic'):
+    """Write a slack markdown section.
+
+    Args:
+        message (str): Message
+        image_url (str): Image URL.
+        alt_text (str): Image alternative text.
+
+    Returns:
+        dict: Block message
+    """
+    block = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": message
+        }
+    }
+
+    if image_url:
+        block['accessory'] = {
+            "type": "image",
+            "image_url": image_url,
+            "alt_text": alt_text
+        }
+
+    return block
--- src/clockzy/lib/slack/slack_messages.py
+++ src/clockzy/lib/slack/slack_messages.py
@@ -1,4 +1,8 @@
-def build_sucess_message(message):
+from clockzy.lib.slack import slack_block_builder as bb
+from clockzy.lib.db.database_interface import get_config_object
+
+
+def build_success_message(message):
     """Build a slack success text message.
 
     Args:
@@ -7,7 +11,7 @@ def build_sucess_message(message):
     Returns:
         str: Message to send with checkmarks icons.
     """
-    return f":white_check_mark: {message} :white_check_mark:"
+    return f"*Status*: _SUCCESS_ :white_check_mark:\n*Message*: {message}"
 
 
 def build_error_message(message):
@@ -19,14 +23,67 @@ def build_error_message(message):
     Returns:
         str: Message to send with X icons.
     """
-    return f":x: {message} :x:"
+    return f"*Status*: _ERROR_ :x:\n*Message*: {message}"
+
+
+def build_block_message(title, description, success=True, image_url=None):
+    """Build a standard block slack message.
+
+    Args:
+        title (str): Message title (header).
+        description (str): Message description (header).
+        success (boolean): Operation status.
+        image_url (str): Image URL to render an image in the message (optional).
+
+    Returns:
+        list(dict): Messages blocks.
+    """
+    header = f":white_check_mark: {title} :white_check_mark:" if success else f":x: {title} :x:"
+    status = 'SUCCESS' if success else 'FAILED'
+    message = f"{header}\n *Status*: {status}\n *Description*: {description}\n"
+
+    block = []
+    block.append(bb.write_slack_divider())
+    block.append(bb.write_slack_markdown(message, image_url))
+    block.append(bb.write_slack_divider())
+
+    return block
+
+
+def build_successful_clocking_message(user_id, clock_action, clock_date_time, user_name):
+    """Build the slack message when an user has clocked successfully.
+
+    Args:
+        user_id (str): User identifier.
+        clock_action (str): Action clocked.
+        clock_date_time (str): Clock date time
+        user_name (str): User name.
+
+    Returns:
+        list(dict): Messages blocks.
+    """
+    user_config = get_config_object(user_id)
+    intratime_integration = False if user_config is None else user_config.intratime_integration
+    image_base_name = f"{clock_action}_intratime.png" if intratime_integration else f"{clock_action}_clockzy.png"
+    image_url = f"https://raw.githubusercontent.com/jmv74211/tools/master/images/repository/clockzy/{image_base_name}"
+    message = ':white_check_mark: Your clocking has been registered successfully :white_check_mark:\n' \
+              f"*Username*: {user_name}\n*Action*: {clock_action.upper()}\n*Datetime*: {clock_date_time}"
+    block = []
+    block.append(bb.write_slack_divider())
+    block.append(bb.write_slack_markdown(message, image_url))
+    block.append(bb.write_slack_divider())
+
+    return block
+
+
+ERROR_IMAGE = 'https://raw.githubusercontent.com/jmv74211/tools/master/images/repository/clockzy/x.png'
 
 # Add user
-ADD_USER_SUCCESS = build_sucess_message('The user has been created successfully')
+ADD_USER_SUCCESS = build_success_message('The user has been created successfully')
 USER_ALREADY_REGISTERED = build_error_message('Your user is already registered!')
 ADD_USER_ERROR = build_error_message('Could not create the user. Please contact with the app administrator')
 
 # Delete user
-DELETE_USER_SUCCESS = build_sucess_message('The user has been deleted successfully')
+DELETE_USER_SUCCESS = build_success_message('The user has been deleted successfully')
 USER_NOT_REGISTERED = build_error_message('Your user is not registered!. You can do it typing `/sign_up` command')
 DELETE_USER_ERROR = build_error_message('Could not delete the user. Please contact with the app administrator')
